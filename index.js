00ace032a7f4e46cdf8abd3525714a3ca274495e
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-10-29 13:50:14 +0700

    added update function for video, for key and tags. updated package.json for additional requirement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Express = require ('express')
 const App = Express ()
 const Request = require ('request')
+const BodyParser = require('body-parser')
 const ZiggeoSdk = require ('ziggeo')
 const Fs = require ('fs')
 
@@ -10,6 +11,8 @@ ZiggeoSdk.init (process.env.API_TOKEN, process.env.PRIVATE_KEY)
 
 App.engine ('ejs', require('ejs-locals'));
 App.set ('view engine', 'ejs')
+App.use(BodyParser.json())
+App.use(BodyParser.urlencoded({extended: true}))
 
 App.get ('/', function (req, res) {
   	res.render('home', {
@@ -44,8 +47,32 @@ App.get ('/video/:videoId', function (req, res){
 		ziggeo_api_token: process.env.API_TOKEN,
 		page_title: 'Single video via Ziggeo version 2',
 		video_id: video_id,
-		need_ziggeo: 1
+		need_ziggeo: 1,
+		update_result: 0
 	})
+})
+
+App.post ('/video/:videoId', function (req, res){
+	var video_id = req.params.videoId
+	var tags = req.body.tags
+	var key = req.body.key
+	var updateArguments = {}
+	if(tags !== '' && tags !== undefined){
+		updateArguments.tags = tags
+	}
+	if(key !== '' && key !== undefined){
+		updateArguments.key = key
+	}
+	ZiggeoSdk.Videos.update(video_id,updateArguments, function(){
+		res.render ('video',{
+			ziggeo_api_token: process.env.API_TOKEN,
+			page_title: 'Single video via Ziggeo version 2',
+			video_id: video_id,
+			need_ziggeo: 1,
+			update_result: 'success'
+		})	
+	})
+	
 })
 
 App.get ('/video/:videoId/stream/:stream_id', function (req, res){
